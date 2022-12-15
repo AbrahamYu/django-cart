861d80b8a03ce65930ac784ce2ce31a26045a845
AbrahamYu <cpttkrjin@gmail.com>
2022-12-15 17:01:25 +0900

    third push project

--- store/models.py
+++ store/models.py
@@ -25,6 +25,13 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+    def size(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+
 variation_category_choice = (
     ('color', 'color'),
     ('size','size'),
@@ -37,8 +44,10 @@ class Variation(models.Model):
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now=True)
 
-    def __unicode__(self):
-        return self.product
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
     
 
 
